77e52d1a6a81a68abb759a63f6c417237ca0a4c7
A-Bong <tychejin@naver.com>
2022-09-13 17:08:54 +0900

    Feat: [Spring Boot]  REST API 만들기(3) - Logback 설정

--- src/main/java/com/example/springbootrestapi/web/controller/TodoController.java
+++ src/main/java/com/example/springbootrestapi/web/controller/TodoController.java
@@ -6,20 +6,20 @@ import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RequiredArgsConstructor
 @RestController
 public class TodoController {
 
   private final TodoService todoService;
 
-  /**
-   * To-Do 조회
-   */
+  /** To-Do 조회 */
   @PostMapping(
       value = "/api/todos",
       consumes = MediaType.APPLICATION_JSON_VALUE,
@@ -28,6 +28,7 @@ public class TodoController {
       HttpServletRequest request,
       HttpServletResponse response,
       @RequestBody Todo.Request todoRequest) {
+    log.info("todoRequest:[{}]", todoRequest.toString());
     return todoService.getTodos(todoRequest);
   }
 }
