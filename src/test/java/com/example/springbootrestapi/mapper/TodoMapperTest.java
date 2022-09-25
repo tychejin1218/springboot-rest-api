8559f4c9687b01dedaefaeddf6a4ca9318e695e4
A-Bong <tychejin@naver.com>
2022-09-25 22:11:20 +0900

    Feat: [Spring Boot]  REST API 만들기(5) - Mapper 구현 및 단위 테스트(Junit5)

---
+++ src/test/java/com/example/springbootrestapi/mapper/TodoMapperTest.java
@@ -0,0 +1,40 @@
+package com.example.springbootrestapi.mapper;
+
+import com.example.springbootrestapi.config.DataSourceConfiguration;
+import com.example.springbootrestapi.domain.Todo;
+import com.example.springbootrestapi.domain.Todo.Response;
+import com.example.springbootrestapi.mapper.TodoMapper;
+import java.util.List;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@Slf4j
+@SpringBootTest
+@ActiveProfiles("local")
+class TodoMapperTest {
+
+  @Autowired
+  TodoMapper todoMapper;
+
+  @Disabled("TodoMapper_To-Do 조회")
+  @Test
+  void testGetTodos(){
+
+    // Given
+    Todo.Request todoRequest = Todo.Request.builder()
+        .title("Spring Cloud")
+        .description("description")
+        .build();
+
+    // When
+    List<Todo.Response> todoResponse = todoMapper.getTodos(todoRequest);
+
+    // Then
+    Assertions.assertEquals(2, todoResponse.size());
+  }
+}
